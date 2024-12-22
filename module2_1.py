8ceab27eec056e56a84b102346c8bcd9d2281a38
RiLo-p <wiwer.alemoure@gmail.com>
2024-12-22 23:33:58 +0300

    Add files via upload

---
+++ module2_1.py
@@ -0,0 +1,10 @@
+number1=int(input('Введите первое число: '))
+number2=int(input('Введите второе число: '))
+number3=int(input('Введите третье число: '))
+if number1==number2 and number1==number3:
+    print('3')
+elif number1==number2 or number1==number3 or number2==number3:
+    print('2')
+elif number1!=number2 and number1!=number3 and number2!=number3:
+    print('0')
+
