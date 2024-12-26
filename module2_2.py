22ad5cc09ee70f56b49ce6ee3f6357a406ae40c6
RiLo-p <wiwer.alemoure@gmail.com>
2024-12-26 23:22:53 +0300

    Add files via upload

---
+++ module2_2.py
@@ -0,0 +1,10 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+id = 0
+while id < len(my_list):
+    if my_list[id] >= 0:
+        if my_list[id] != 0:
+            print(my_list[id])
+        id = id + 1
+        continue
+    else:
+        break
